b62315897e2f27f945919e2a0a67f6725850a7f6
vai <ncysatnaf@gmail.com>
2017-01-14 13:08:24 +0800

    rustとgo |でノードに似ている

---
+++ basic/print.rs
@@ -0,0 +1,19 @@
+fn main() {
+  println!("{} days", 31)
+
+  println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob")
+
+  println!("{subject} {verb} {object}", object="the lazy dog", subject="the quick brown fox", verb="jumps over")
+
+  println!("{number:>width}", number=1, width=6)
+
+  println!("{number:>0width$}", number=1, width=6)
+
+  println!("My name is {0}, {1} {0}", "Bond", "James")
+
+  #[allow(dead_code)]
+
+  struct Structure(i32)
+
+  // println!("This struct `{}` won't print..", Structure(3))
+}
